928e57abb0bfcf7d8c0858f2efb05ab29ae42162
Bukinich-Pavel <bukinich98@mail.ru>
2022-02-10 00:35:03 +0300

    Begin work logIn

--- Purpose/Controllers/AccountController.cs
+++ Purpose/Controllers/AccountController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Purpose.Models;
 using Purpose.ViewModels;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace Purpose.Controllers
@@ -19,25 +20,54 @@ namespace Purpose.Controllers
 
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<StatusCodeResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await signInManager.PasswordSignInAsync(model.Email, model.Password, true, false);
+                if (result.Succeeded)
+                {
+                    
+                    //Response.Body = 
+                    return new OkResult();
+                }
+            }
+            return new BadRequestResult();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<StatusCodeResult> Logout()
+        {
+            await signInManager.SignOutAsync();  // удаляем аутентификационные куки
+            return new OkResult();
+        }
+
+
+        [HttpPost]
         public async Task<StatusCodeResult> Register(RegisterViewModel model)
         {
-            //if (ModelState.IsValid)
-            //{
-            //    var userName = model.Email.Substring(model.Email.IndexOf('@'));
-            //    User user = new User {  Email = model.Email, 
-            //                            UserName = userName,
-            //                            FirstName = model.FirstName,
-            //                            SecondName = model.SecondName,
-            //                            Year = model.Year
-            //    };
+            if (ModelState.IsValid)
+            {
+                var userName = model.Email.Substring(model.Email.IndexOf('@'));
+                User user = new User
+                {
+                    Email = model.Email,
+                    UserName = userName,
+                    FirstName = model.FirstName,
+                    SecondName = model.SecondName,
+                    Year = model.Year
+                };
 
-            //    IdentityResult result = await userManager.CreateAsync(user, model.Password);  // добавляем пользователя
-            //    if (result.Succeeded)
-            //    {
-            //        await signInManager.SignInAsync(user, false);  // установка куки
-            //        return new OkResult();
-            //    }
-            //}
+                IdentityResult result = await userManager.CreateAsync(user, model.Password);  // добавляем пользователя
+                if (result.Succeeded)
+                {
+                    await signInManager.SignInAsync(user, false);  // установка куки
+                    return new OkResult();
+                }
+            }
             return new BadRequestResult();
         }
     }
