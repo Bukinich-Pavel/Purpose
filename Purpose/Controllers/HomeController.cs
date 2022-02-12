9f562d59d173184486e9a0230b5a99247f716100
Bukinich-Pavel <bukinich98@mail.ru>
2022-02-12 23:40:24 +0300

    Home/Index return json user

--- Purpose/Controllers/HomeController.cs
+++ Purpose/Controllers/HomeController.cs
@@ -3,26 +3,35 @@ using Purpose.Models;
 using Purpose.Cloud;
 using System;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using Purpose.ViewModels;
 
 namespace Purpose.Controllers
 {
     public class HomeController : Controller
     {
         private ApplicationContext db;
-        public HomeController(ApplicationContext db)
+        private readonly UserManager<User> userManager;      // use for add user
+
+        public HomeController(ApplicationContext db, UserManager<User> userManager)
         {
             this.db = db;
+            this.userManager = userManager;
         }
 
         [HttpGet]
         //[Authorize]
-        public IActionResult Index()
+        public async Task<JsonResult> Index()
         {
             if (User.Identity.IsAuthenticated)
             {
-                return Content(User.Identity.Name);
+                var user = await userManager.FindByEmailAsync(User.Identity.Name);
+                Response.StatusCode = 200;
+                var userFront = new UserFrontViewModel(user);  //Create "User for Frontend" from "User from DB"
+                return Json(new ResponseViewModel(userFront)); //Send User
             }
-            return Content("хуй");
+            return Json(new ResponseViewModel(false)); //Send status:false
         }
 
         [HttpPost]
